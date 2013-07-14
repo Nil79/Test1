d6df4619d7aa39c102d9089c90e44ca10810b136
Nil79 <sodano.n@gmail.com>
2013-07-14 21:57:46 +0200

    Added solution

---
+++ Test1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is an example test
+            Console.WriteLine("Hello World");
+        }
+    }
+}
